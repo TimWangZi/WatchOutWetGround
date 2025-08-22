ea68b6dabb9b0ed96b69f3f4487d9fe1e3c5a0ce
TimWangZi <53970763+TimWangZi@users.noreply.github.com>
2025-08-23 00:11:33 +0800

    2025/8/23 0:11

---
+++ watch_out_wet_ground/Object/Player/main_player.gd
@@ -0,0 +1,34 @@
+extends RigidBody2D
+
+@export var FORCE_LEN = 1.0 # 当按下键盘上的按键时，对玩家施加力的大小
+var flip_or_not = false
+var animate_sprite:AnimatedSprite2D
+
+func _ready():
+	animate_sprite = $AnimatedSprite2D
+	animate_sprite.play("idle")
+
+func _process(delta):
+	var force = Vector2.ZERO
+	if Input.is_action_pressed("player_move_up"):
+		force.y -= 1.0
+	if Input.is_action_pressed("player_move_down"):
+		force.y += 1.0
+	if Input.is_action_pressed("player_move_left"):
+		force.x -= 1.0
+	if Input.is_action_pressed("player_move_right"):
+		force.x += 1.0
+	force = force.normalized() * FORCE_LEN # 力归一化并乘上力的大小
+	
+	flip_or_not = linear_velocity.x > 0
+	animate_sprite.flip_h = flip_or_not
+	
+	if linear_velocity.length() < 2.0:
+		animate_sprite.play("idle")
+	else:
+		animate_sprite.play("move")
+	
+	if force.length() > 0.0:
+		add_constant_force(force) # 向刚体施加力
+	else:
+		constant_force = Vector2.ZERO # 清空物体的力
